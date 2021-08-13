4d1be8706b1d77049dda14b2ae75891ff98b3437
Nedim <mr.ramicevic@gmail.com>
2021-08-13 10:45:01 +0300

    mapFunction

--- src/colorPicker.js
+++ src/colorPicker.js
@@ -1,25 +1,20 @@
 import React, {useState} from 'react';
 
+const colorNames = ['Aquamarine', 'BlueViolet', 'Chartreuse', 'CornflowerBlue', 'Thistle', 'SpringGreen', 'SaddleBrown', 'PapayaWhip', 'MistyRose'];
+
 export default function ColorPicker() {
-  const [color, setColor] = useState('purple')
+  const [color, setColor] = useState(colorNames[0])
 
  const divStyle = {backgroundColor: color};
 
  return (
     <div style={divStyle}>
       <p>The color is {color}</p>
-      <button onClick = {() => setColor('Aquamarine')}>
-        Aquamarine
-      </button>
-      <button onClick = {() => setColor("BlueViolet")}>
-        BlueViolet
-      </button>
-      <button onClick = {() => setColor("Chartreuse")}>
-        Chartreuse
-      </button>
-      <button onClick = {() => setColor("CornflowerBlue")}>
-        CornflowerBlue
-      </button>
+      {
+          colorNames.map(colorName => (
+              <button onClick ={() => setColor(colorName)} key= {colorName}>{colorName}</button>
+          ))
+      }
     </div>
   );
 }
